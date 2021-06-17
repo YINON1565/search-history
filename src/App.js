4496923607efbdf611e98688a34a937abe59d7af
Yinon_Eliyahu <yinoneliyaoo@gmail.com>
2021-06-17 09:51:29 +0300

    replace dedalte content in  my search box under the cover of "Recoil"

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
 import './App.scss';
 
+import React from 'react';
+import { RecoilRoot } from 'recoil';
+import { SearchBox } from './components/search-box/search-box.component';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <RecoilRoot>
+        <SearchBox />
+    </RecoilRoot>
   );
 }
 
