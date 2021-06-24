5d43f54574744503061e83276a7f8dbc66123ab9
Yinon_Eliyahu <yinoneliyaoo@gmail.com>
2021-06-24 18:19:11 +0300

    bold Autocomplate Pipe  by find index and Using "substring"

--- src/util/helpers.tsx
+++ src/util/helpers.tsx
@@ -1,5 +1,3 @@
-const SPLITTER = "#%%#@#";
-
 // ******* function Helpers *********
 const isSame = (str_1: string, str_2: string) => {
   return str_1 === cleanStr(str_2);
@@ -9,22 +7,16 @@ const cleanStr = (str: string) => {
   return str.toLowerCase().trim();
 };
 
-const boldAutocomplatePipe = (search: string, term: string): JSX.Element[] => {
+const boldAutocomplatePipe = (search: string, term: string): JSX.Element => {
   const cleanTerm = cleanStr(term);
-  return search
-    .replace(cleanTerm, SPLITTER + cleanTerm + SPLITTER)
-    .split(SPLITTER)
-    .map((partSearch, i) => {
-      return partSearch === cleanTerm ? (
-        <span key={i}>{cleanTerm}</span>
-      ) : (
-        <b key={i}>{partSearch}</b>
-      );
-    });
+  const startIdx = search.indexOf(cleanTerm);
+  return (
+    <>
+      <b>{search.substring(0, startIdx)}</b>
+      <span>{cleanTerm}</span>
+      <b>{search.substring(startIdx + cleanTerm.length)}</b>
+    </>
+  );
 };
 
-export {
-  cleanStr,
-  isSame,
-  boldAutocomplatePipe
-};
+export { cleanStr, isSame, boldAutocomplatePipe };
